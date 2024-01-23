7cbe1fc2506087e402082db4dbaac4f25e394600
je <envyxbot@gmail.com>
2024-01-23 17:41:44 +0800

    dependency update:
    - add hilt-navigation for DI
    - add retrofit-converter-gson for converting response request
    - add okhttp-logging for logging response request body
    - add lifecycle-runtime-compose for viewmodel state

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 
     // Hilt dependency
     implementation("com.google.dagger:hilt-android:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
 
     // Arrow.kt dependency
@@ -64,13 +65,19 @@ dependencies {
 
     // Retrofit dependency
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // OkHttp dependency
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    // dependency for collectAsStateWithLifecycle()
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
