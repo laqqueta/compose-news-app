aa9cc1b8d6f3e3f5839f933868d6ed0008867382
je <envyxbot@gmail.com>
2024-01-23 23:45:29 +0800

    update: add newsPage param

--- app/src/main/java/com/pbi/newsapp/ui/news/view/NewsViewState.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/NewsViewState.kt
@@ -5,5 +5,6 @@ import com.pbi.newsapp.domain.model.News
 data class NewsViewState(
     val isLoading: Boolean = false,
     val news: News? = null,
+    val newsPage: Int = 0,
     val error: String? = null
 )
