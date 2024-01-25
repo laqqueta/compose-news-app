17570f3f55574c9543bef10403a7e37d0744a95b
je <envyxbot@gmail.com>
2024-01-25 22:59:05 +0800

    feat: add paging source implementation for headlines endpoint

---
+++ app/src/main/java/com/pbi/newsapp/data/paging/HeadlinesPagingSource.kt
@@ -0,0 +1,54 @@
+package com.pbi.newsapp.data.paging
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import arrow.core.Either
+import com.pbi.newsapp.domain.model.Article
+import com.pbi.newsapp.domain.model.request.TopHeadlinesEndpointParam
+import com.pbi.newsapp.domain.repository.NewsRepository
+import com.pbi.newsapp.utils.Event
+import com.pbi.newsapp.utils.sendEvent
+import javax.inject.Inject
+
+class HeadlinesPagingSource@Inject constructor(
+    private val repository: NewsRepository,
+) : PagingSource<Int, Article>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val page = params.key ?: 1
+        val param = TopHeadlinesEndpointParam(
+            country = "id",
+            pageSize = params.loadSize,
+            page = page
+        )
+
+        Log.i("api-paging-headlines-source-page", "page: $page")
+
+        return when(val response = repository.getHeadlines(param.toHashMap())) {
+            is Either.Left -> {
+                response.onLeft {
+                    sendEvent(Event.ToastMessage(it.error.message))
+                }
+                // pass Throwable to play safe
+                LoadResult.Error(response.leftOrNull()?.t ?: Throwable(message = "Unknown Error"))
+            }
+            is Either.Right -> {
+                var data: List<Article> = emptyList()
+
+                response.onRight {
+                    data = it.articles
+                    Log.i("api-paging-news-headlines-status", "status: ${it.status}")
+                }
+
+                LoadResult.Page(
+                    data = data,
+                    prevKey = if (page == 1) null else page.minus(1),
+                    nextKey = if (data.isEmpty()) null else page.plus(1),
+                )
+            }
+        }
+    }
+}
