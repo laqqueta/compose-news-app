5be7d597c48e8b5ca57f06f9baa4a959a238b6b7
je <envyxbot@gmail.com>
2024-01-25 22:58:08 +0800

    test: paging source for /top-headlines endpoint

--- app/src/main/java/com/pbi/newsapp/ui/news/view/NewsScreen.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/NewsScreen.kt
@@ -1,17 +1,23 @@
 package com.pbi.newsapp.ui.news.view
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.log
 import com.pbi.newsapp.domain.model.Article
 import com.pbi.newsapp.ui.news.viewmodel.NewsViewModel
 import com.pbi.newsapp.ui.util.component.LoadingDialog
@@ -20,31 +26,41 @@ import com.pbi.newsapp.ui.util.component.LoadingDialog
 internal fun NewsScreen(
     viewModel: NewsViewModel = hiltViewModel()
 ) {
-    val newsPaging = viewModel.articlePaging.collectAsLazyPagingItems()
+    val everythingPagingData = viewModel.everythingPaging.collectAsLazyPagingItems()
+    val headlinesPagingData = viewModel.headlinesPaging.collectAsLazyPagingItems()
 
-    NewsContent(newsPaging)
+    NewsContent(everythingPagingData, headlinesPagingData)
 }
 
 @Composable
 fun NewsContent(
-    pager: LazyPagingItems<Article>
+    pagerEverything: LazyPagingItems<Article>,
+    pagerHeadlines: LazyPagingItems<Article>
 ) {
     Column {
-        Button(onClick = {}) {
-            Text(text = "Update News to Next Page")
-        }
+//        LazyRow {
+//            items(pagerHeadlines.itemCount) {
+//                Text(text = pagerHeadlines[it]?.title ?: "empty", modifier = Modifier.padding(end = 20.dp))
+//            }
+//        }
 
+//        Spacer(modifier = Modifier.padding(bottom = 50.dp))
         LazyColumn {
-            items(pager.itemCount) {
-                Text(text = pager[it]?.content ?: "empty", modifier = Modifier.padding(bottom = 20.dp))
+            items(pagerEverything.itemCount) {
+                Text(text = pagerEverything[it]?.content ?: "empty", modifier = Modifier.padding(bottom = 20.dp))
             }
 
-            pager.apply {
+            pagerEverything.apply {
                 when {
-                    loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading ->  {
+                    loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading -> {
+                        //You can add modifier to manage load state when first time response page is loading
                         item {
                             LoadingDialog()
+                            Log.i("loading-dialog", "showed")
                         }
+                    }
+                    loadState.append is LoadState.Error -> {
+                        //You can use modifier to show error message
                     }
                 }
             }
