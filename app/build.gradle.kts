50e1c75770daa38b27c826b874627beefbb35345
je <envyxbot@gmail.com>
2024-01-26 23:21:47 +0800

    dependency update: add Coil.kt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,13 +77,15 @@ dependencies {
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
     implementation("androidx.paging:paging-compose:3.3.0-alpha02")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    //Coin dependency
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
     // dependency for collectAsStateWithLifecycle()
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
