d4afbe072387b3696b479af0fd810039a3fa3e01
je <envyxbot@gmail.com>
2024-01-26 23:23:29 +0800

    finishing paging

--- app/src/main/java/com/pbi/newsapp/ui/news/view/NewsScreen.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/NewsScreen.kt
@@ -2,19 +2,29 @@ package com.pbi.newsapp.ui.news.view
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.pbi.newsapp.domain.model.Article
 import com.pbi.newsapp.ui.news.viewmodel.NewsViewModel
 import com.pbi.newsapp.ui.util.component.LoadingDialog
+import com.pbi.newsapp.utils.Endpoint
+import dagger.Lazy
 
 @Composable
 internal fun NewsScreen(
@@ -22,42 +32,65 @@ internal fun NewsScreen(
 ) {
     val everythingPagingData = viewModel.everythingPaging.collectAsLazyPagingItems()
     val headlinesPagingData = viewModel.headlinesPaging.collectAsLazyPagingItems()
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
-    NewsContent(everythingPagingData, headlinesPagingData)
+    NewsContent(
+        everythingPagingData,
+        headlinesPagingData,
+        state,
+        { viewModel.toNews() },
+        { viewModel.toHeadlines() }
+    )
 }
+
 
 @Composable
 fun NewsContent(
-    pagerEverything: LazyPagingItems<Article>,
-    pagerHeadlines: LazyPagingItems<Article>
+    newsPagingData: LazyPagingItems<Article>,
+    headlinesPagingData: LazyPagingItems<Article>,
+    state: NewsViewState,
+    newsOnClick: () -> Unit,
+    headlinesOnClick: () -> Unit
 ) {
     Column {
-//        LazyRow {
-//            items(pagerHeadlines.itemCount) {
-//                Text(text = pagerHeadlines[it]?.title ?: "empty", modifier = Modifier.padding(end = 20.dp))
-//            }
-//        }
 
-//        Spacer(modifier = Modifier.padding(bottom = 50.dp))
-        LazyColumn {
-            items(pagerEverything.itemCount) {
-                Text(text = pagerEverything[it]?.content ?: "empty", modifier = Modifier.padding(bottom = 20.dp))
-            }
+        Button(onClick = newsOnClick) {
+            Text(text = "News Page")
+        }
 
-            pagerEverything.apply {
-                when {
-                    loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading -> {
-                        //You can add modifier to manage load state when first time response page is loading
-                        item {
-                            LoadingDialog()
-                            Log.i("loading-dialog", "showed")
-                        }
+        Button(onClick = headlinesOnClick) {
+            Text(text = "Headlines Page")
+        }
+
+        when(state.endpoint) {
+            Endpoint.EVERYTHING -> ArticleView(pagingData = newsPagingData)
+            else -> ArticleView(pagingData = headlinesPagingData)
+        }
+
+    }
+}
+
+@Composable
+fun ArticleView(pagingData: LazyPagingItems<Article>) {
+    LazyColumn {
+        items(pagingData.itemCount) {
+            Text(text = pagingData[it]?.title ?: "empty", modifier = Modifier.padding(bottom = 20.dp))
+        }
+
+        pagingData.apply {
+            when {
+                loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading -> {
+                    //You can add modifier to manage load state when first time response page is loading
+                    item {
+                        LoadingDialog()
+                        Log.i("loading-dialog", "showed")
                     }
-                    loadState.append is LoadState.Error -> {
-                        //You can use modifier to show error message
-                    }
+                }
+                loadState.append is LoadState.Error -> {
+                    //You can use modifier to show error message
                 }
             }
         }
+
     }
 }
--- app/src/main/java/com/pbi/newsapp/ui/news/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/viewmodel/NewsViewModel.kt
@@ -1,5 +1,6 @@
 package com.pbi.newsapp.ui.news.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -9,9 +10,12 @@ import androidx.paging.cachedIn
 import com.pbi.newsapp.data.paging.EverythingPagingSource
 import com.pbi.newsapp.data.paging.HeadlinesPagingSource
 import com.pbi.newsapp.domain.model.Article
+import com.pbi.newsapp.ui.news.view.NewsViewState
+import com.pbi.newsapp.utils.Endpoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,11 +31,14 @@ class NewsViewModel @Inject constructor(
     private val _headlinesPaging: MutableStateFlow<PagingData<Article>> =
         MutableStateFlow(PagingData.empty())
 
+    private val _state = MutableStateFlow(NewsViewState())
     var everythingPaging = _everythingPaging.asStateFlow()
         private set
 
     var headlinesPaging = _headlinesPaging.asStateFlow()
         private set
+
+    val state = _state.asStateFlow()
 
     init {
         everythingPagingSource()
@@ -42,7 +49,7 @@ class NewsViewModel @Inject constructor(
         viewModelScope.launch {
             Pager(
                 config = PagingConfig(
-                    10, enablePlaceholders = true
+                    3, enablePlaceholders = true
                 )
             ) {
                 everythingSource
@@ -57,13 +64,32 @@ class NewsViewModel @Inject constructor(
         viewModelScope.launch {
             Pager(
                 config = PagingConfig(
-                    10, enablePlaceholders = true
+                    3, enablePlaceholders = true
                 )
             ) {
                 headlinesSource
             }.flow.cachedIn(viewModelScope).collect {
                 _headlinesPaging.value = it
             }
+        }
+    }
+
+    fun toNews() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(endpoint = Endpoint.EVERYTHING)
+            }
+            Log.i("viewmodel-update-news", "state: ${_state.value.endpoint}")
+        }
+    }
+
+
+    fun toHeadlines() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(endpoint = Endpoint.HEADLINES)
+            }
+            Log.i("viewmodel-update-article", "state: ${_state.value.endpoint}")
         }
     }
 
