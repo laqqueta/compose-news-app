24804829dcb08108ecb6d8908f1cbfca14c8ffc4
je <envyxbot@gmail.com>
2024-01-28 21:10:19 +0800

    modified file

--- app/src/main/java/com/pbi/newsapp/utils/Constant.kt
+++ app/src/main/java/com/pbi/newsapp/utils/Constant.kt
@@ -2,6 +2,5 @@ package com.pbi.newsapp.utils
 
 object Constant {
     const val BASE_URL = "https://newsapi.org/v2/"
-//    const val API_KEY = "" // Laqquetavern api key
-    const val API_KEY = "" // i.yuskamal17 api key
+    const val API_KEY = ""
 }
