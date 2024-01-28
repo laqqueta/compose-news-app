d2ac8c6130f54c59fa82fb2e8773aa7a787d0a67
je <envyxbot@gmail.com>
2024-01-28 21:07:51 +0800

    add helvetica font

---
+++ app/src/main/java/com/pbi/newsapp/ui/util/component/FontFamily.kt
@@ -0,0 +1,12 @@
+package com.pbi.newsapp.ui.util.component
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.pbi.newsapp.R
+
+val helveticaFamily = FontFamily(
+    Font(R.font.helvetica_roman, FontWeight.Normal),
+    Font(R.font.helvetica_medium, FontWeight.Medium),
+    Font(R.font.helvetica_bold_italic, FontWeight.Bold),
+)
