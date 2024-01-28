799f0e762e6767c947ee8f385c093c56124f1e0c
je <envyxbot@gmail.com>
2024-01-28 21:06:55 +0800

    fix null value on urlImage

--- app/src/main/java/com/pbi/newsapp/domain/model/Article.kt
+++ app/src/main/java/com/pbi/newsapp/domain/model/Article.kt
@@ -6,7 +6,7 @@ data class Article(
     val title: String,
     val description: String,
     val url: String,
-    val urlImage: String,
+    val urlToImage: String,
     val publishedAt: String,
     val content: String
 )
