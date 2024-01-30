f13949e349c330322046ca51fea4ffc992bac65f
je <envyxbot@gmail.com>
2024-01-30 13:14:37 +0800

    fix warnings

--- app/src/main/java/com/pbi/newsapp/utils/Endpoint.kt
+++ app/src/main/java/com/pbi/newsapp/utils/Endpoint.kt
@@ -4,5 +4,4 @@ package com.pbi.newsapp.utils
 enum class Endpoint {
     EVERYTHING,
     HEADLINES,
-    SOURCE,
 }
--- app/src/main/java/com/pbi/newsapp/utils/NewsPagingEvent.kt
+++ app/src/main/java/com/pbi/newsapp/utils/NewsPagingEvent.kt
@@ -1,8 +1,5 @@
 package com.pbi.newsapp.utils
 
-import androidx.paging.PagingSource
-import com.pbi.newsapp.domain.model.Article
-
-suspend fun PagingSource<Int, Article>.sendEvent(event: Any) {
+suspend fun sendEvent(event: Any) {
     EventBus.sendEvent(event)
 }
