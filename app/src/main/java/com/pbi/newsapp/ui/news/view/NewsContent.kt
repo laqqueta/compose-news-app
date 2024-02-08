eadf96d15f4bd6ace039ea631452f0960c0c704b
je <envyxbot@gmail.com>
2024-02-08 21:10:22 +0800

    refactor file

--- app/src/main/java/com/pbi/newsapp/ui/news/view/NewsContent.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/NewsContent.kt
@@ -1,12 +1,14 @@
 package com.pbi.newsapp.ui.news.view
 
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.paging.compose.LazyPagingItems
 import com.pbi.newsapp.R
@@ -23,33 +25,39 @@ fun NewsContent(
     state: NewsViewState,
     newsOnClick: () -> Unit,
     headlinesOnClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Scaffold(bottomBar = {
-        BottomAppBar(
-            containerColor = Color(0xFF1b1b1c),
-            modifier = Modifier
-                .height(
-                    dimensionResource(id = R.dimen.bottom_app_bar_height)
-                )
-                .padding(
-                    dimensionResource(id = R.dimen.bottom_app_bar_padding)
-                ),
-            actions = {
-                BottomBarButton(
-                    focusRequester = state.newsFocus,
-                    icon = state.newsIcon,
-                    modifier = Modifier.weight(1f),
-                    onClick = newsOnClick
-                )
-                BottomBarButton(
-                    focusRequester = state.headlinesFocus,
-                    icon = state.headlinesIcon,
-                    modifier = Modifier.weight(1f),
-                    onClick = headlinesOnClick
-                )
-            }
-        )
-    }) {
+    Scaffold(
+        modifier = Modifier.navigationBarsPadding(),
+        bottomBar = {
+            BottomAppBar(
+                modifier = modifier
+                    .height(
+                        dimensionResource(id = R.dimen.bottom_app_bar_height)
+                    )
+                    .padding(
+                        dimensionResource(id = R.dimen.bottom_app_bar_padding)
+                    )
+                ,
+                actions = {
+                    BottomBarButton(
+                        focusRequester = state.newsFocus,
+                        icon = state.newsIcon,
+                        modifier = Modifier
+                            .weight(1f),
+                        onClick = newsOnClick
+                    )
+                    BottomBarButton(
+                        focusRequester = state.headlinesFocus,
+                        icon = state.headlinesIcon,
+                        modifier = Modifier
+                            .weight(1f),
+                        onClick = headlinesOnClick
+                    )
+                }
+            )
+        }
+    ) {
         when(state.endpoint) {
             Endpoint.EVERYTHING -> {
                 LoadingAnimation(pagingData = newsPagingData)
