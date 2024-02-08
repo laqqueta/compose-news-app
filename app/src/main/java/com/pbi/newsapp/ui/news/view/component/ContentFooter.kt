cb774b28bd4ce10a076bdc96f9e31c5cb394f461
je <envyxbot@gmail.com>
2024-02-08 18:48:34 +0800

    apply Typography theme

--- app/src/main/java/com/pbi/newsapp/ui/news/view/component/ContentFooter.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/component/ContentFooter.kt
@@ -4,15 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import com.pbi.newsapp.R
-import com.pbi.newsapp.ui.util.component.helveticaFamily
 
 @Composable
 fun NewsFooter(
@@ -30,9 +28,7 @@ fun NewsFooter(
     ) {
         Text(
             text = "Source: $source",
-            fontFamily = helveticaFamily,
-            fontWeight = FontWeight.Normal,
-            fontSize = 13.sp
+            style = MaterialTheme.typography.displaySmall
         )
     }
 }
