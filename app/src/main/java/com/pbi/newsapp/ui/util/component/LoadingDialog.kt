7bd9792f95be9de8c6e0d4d07b31a9821ddcea57
je <envyxbot@gmail.com>
2024-02-08 21:09:51 +0800

    adjust box for loading dialog

--- app/src/main/java/com/pbi/newsapp/ui/util/component/LoadingDialog.kt
+++ app/src/main/java/com/pbi/newsapp/ui/util/component/LoadingDialog.kt
@@ -1,16 +1,12 @@
 package com.pbi.newsapp.ui.util.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -18,14 +14,12 @@ import androidx.compose.ui.window.DialogProperties
 @Composable
 fun LoadingDialog() {
     Dialog(
-        onDismissRequest = { /*TODO*/ },
+        onDismissRequest = { TODO() },
         properties = DialogProperties(dismissOnClickOutside = false)
     ) {
         Box(
             modifier = Modifier
-                .width(200.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(Color.White),
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             CircularProgressIndicator(
