fd41f345cec28bb6fcf00a11cc43da4c789578cb
je <envyxbot@gmail.com>
2024-02-08 21:08:08 +0800

    add elvis operator to author paramter

--- app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
@@ -3,9 +3,11 @@ package com.pbi.newsapp.ui.news.view.component
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.pbi.newsapp.R
@@ -31,7 +33,7 @@ fun NewsDetails(
                 )
         ) {
             NewsHeader(
-                author = article.author,
+                author = article.author ?: "none@news.api",
                 publishedAt = article.publishedAt,
                 maxLines = 1
             )
