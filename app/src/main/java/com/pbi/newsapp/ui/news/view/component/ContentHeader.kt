bf9fa37aa9c67de40cac838ff7ca127cdf363c3e
je <envyxbot@gmail.com>
2024-02-08 18:48:29 +0800

    apply Typography theme

--- app/src/main/java/com/pbi/newsapp/ui/news/view/component/ContentHeader.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/component/ContentHeader.kt
@@ -9,21 +9,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextOverflow
 import com.pbi.newsapp.R
-import com.pbi.newsapp.ui.util.component.helveticaFamily
 
 
 @Composable
 fun NewsHeader(
     author: String,
     publishedAt: String,
+    maxLines: Int,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -52,9 +52,9 @@ fun NewsHeader(
         ) {
             Text(
                 text = author,
-                fontFamily = helveticaFamily,
-                fontWeight = FontWeight.Medium,
-                fontSize = 13.sp,
+                style = MaterialTheme.typography.displayMedium,
+                maxLines = maxLines,
+                overflow = TextOverflow.Ellipsis,
             )
 
             Spacer(
@@ -65,9 +65,7 @@ fun NewsHeader(
 
             Text(
                 text = publishedAt,
-                fontFamily = helveticaFamily,
-                fontWeight = FontWeight.Medium,
-                fontSize = 10.sp
+                style = MaterialTheme.typography.labelLarge
             )
         }
     }
