e6ac4bca7f86c8d15287c657cb8df1016c2d02be
je <envyxbot@gmail.com>
2024-02-08 21:07:43 +0800

    change article source to nullable

--- app/src/main/java/com/pbi/newsapp/domain/model/Article.kt
+++ app/src/main/java/com/pbi/newsapp/domain/model/Article.kt
@@ -2,7 +2,7 @@ package com.pbi.newsapp.domain.model
 
 data class Article(
     val source: ArticleSource,
-    val author: String,
+    val author: String?,
     val title: String,
     val description: String,
     val url: String,
