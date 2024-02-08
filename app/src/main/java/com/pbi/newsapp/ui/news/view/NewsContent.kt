b17960bbbb9cd9559b3e98591c573f8043158a33
je <envyxbot@gmail.com>
2024-02-08 21:11:14 +0800

    fix warning unused imports

--- app/src/main/java/com/pbi/newsapp/ui/news/view/NewsContent.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/NewsContent.kt
@@ -3,8 +3,6 @@ package com.pbi.newsapp.ui.news.view
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
@@ -3,11 +3,9 @@ package com.pbi.newsapp.ui.news.view.component
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.pbi.newsapp.R
