a3d0d4e6f4491a5c343f1a9e20e4e1960cd4b367
je <envyxbot@gmail.com>
2024-02-08 18:08:29 +0800

    add @Preview annotation

--- app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
+++ app/src/main/java/com/pbi/newsapp/ui/news/view/component/ArticleDescription.kt
@@ -1,14 +1,24 @@
 package com.pbi.newsapp.ui.news.view.component
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import com.pbi.newsapp.R
 import com.pbi.newsapp.domain.model.Article
+import com.pbi.newsapp.ui.news.view.NewsScreen
+import com.pbi.newsapp.ui.theme.NewsAppTheme
 import com.pbi.newsapp.ui.theme.articleBackground
+import com.pbi.newsapp.utils.DataTest
 
 @Composable
 fun NewsDetails(
@@ -17,7 +27,7 @@ fun NewsDetails(
 ) {
     Column(
         modifier = modifier
-            .background(articleBackground)
+            .background(Color.White)
     ) {
         Column(
             modifier = Modifier
@@ -44,5 +54,16 @@ fun NewsDetails(
                 source = article.source.name
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun NewsApp() {
+    val article = DataTest.article
+
+    NewsAppTheme(darkTheme = false) {
+        // Content here
+        NewsDetails(article = article)
     }
 }
